9b72e5930413d44361b2e7a6ffc241cc9898d75f
Pablo Baños López <pablo@besol.es>
2011-09-07 17:10:34 +0200

    Code reformatting

--- lib/rubygems/commands/compile_command.rb
+++ lib/rubygems/commands/compile_command.rb
@@ -2,51 +2,51 @@ require 'rubygems/command'
 require 'rubygems/compiler'
 
 class Gem::Commands::CompileCommand < Gem::Command
-	def initialize
-		super 'compile', 'Create binary gems from gems with extensions',
-			:platform => Gem::Platform::CURRENT,
-			:fat => ""
+  def initialize
+    super 'compile', 'Create binary gems from gems with extensions',
+          :platform => Gem::Platform::CURRENT,
+          :fat => ""
 
-		add_option('-p', '--platform PLATFORM', 'Output platform name') do |value, options|
-			options[:platform] = value
-		end
+    add_option('-p', '--platform PLATFORM', 'Output platform name') do |value, options|
+      options[:platform] = value
+    end
 
-		add_option('-f', '--fat VERSION:RUBY,...', 'Create fat binary (e.g. --fat 1.8:ruby,1.9:ruby19)') do |value, options|
-			options[:fat] = value
+    add_option('-f', '--fat VERSION:RUBY,...', 'Create fat binary (e.g. --fat 1.8:ruby,1.9:ruby19)') do |value, options|
+      options[:fat] = value
     end
 
     add_option('--exclude-source', 'Exclude source extension files (.c and .h files) as well as the extensions themselves (extconf.rb files, etc)') do |value, options|
       options[:exclude_source] = true
     end
-	end
+  end
 
-	def arguments # :nodoc:
-		"GEM       Path of gem to compile"
-	end
+  def arguments # :nodoc:
+    "GEM       Path of gem to compile"
+  end
 
-	def usage # :nodoc:
-		"#{program_name} GEM"
-	end
+  def usage # :nodoc:
+    "#{program_name} GEM"
+  end
 
-	def execute
+  def execute
     gem = options[:args].shift
 
     unless gem then
-			raise Gem::CommandLineError,
-						"Please specify a gem name or file on the command line"
+      raise Gem::CommandLineError,
+            "Please specify a gem name or file on the command line"
     end
 
-		fat_commands = {}
-		options[:fat].split(',').each do |ver_cmd|
-			ver, cmd = ver_cmd.split(':', 2)
-			unless ver =~ /^\d+\.\d+$/ then
-				raise Gem::CommandLineError,
-							"Invalid version string #{ver.dump}"
-			end
-			fat_commands[ver] = cmd
-		end
+    fat_commands = {}
+    options[:fat].split(',').each do |ver_cmd|
+      ver, cmd = ver_cmd.split(':', 2)
+      unless ver =~ /^\d+\.\d+$/ then
+        raise Gem::CommandLineError,
+              "Invalid version string #{ver.dump}"
+      end
+      fat_commands[ver] = cmd
+    end
 
-		Gem::Compiler.compile(gem, options[:platform], fat_commands, options)
-	end
+    Gem::Compiler.compile(gem, options[:platform], fat_commands, options)
+  end
 end
 
--- lib/rubygems/compiler.rb
+++ lib/rubygems/compiler.rb
@@ -8,142 +8,142 @@ require 'shellwords'
 
 class Gem::Compiler
 
-	extend Gem::UserInteraction
+  extend Gem::UserInteraction
 
-	def self.compile(gem, platform = Gem::Platform::CURRENT, fat_commands = {}, opts = {})
-		gem_dir = "#{File.basename(gem)}.build"
-		gem_dir = File.expand_path(gem_dir)
+  def self.compile(gem, platform = Gem::Platform::CURRENT, fat_commands = {}, opts = {})
+    gem_dir = "#{File.basename(gem)}.build"
+    gem_dir = File.expand_path(gem_dir)
 
-		format = Gem::Format.from_file_by_path(gem)
+    format = Gem::Format.from_file_by_path(gem)
 
-		spec = format.spec
+    spec = format.spec
 
     if spec.extensions.empty?
-			raise Gem::Exception, "There are no extensions to build."
-		end
+      raise Gem::Exception, "There are no extensions to build."
+    end
 
-		if spec.platform != Gem::Platform::RUBY
-			raise Gem::Exception, "The package seems to be built already."
-		end
+    if spec.platform != Gem::Platform::RUBY
+      raise Gem::Exception, "The package seems to be built already."
+    end
 
-		dest_path = File.join(gem_dir, spec.require_paths.first)
+    dest_path = File.join(gem_dir, spec.require_paths.first)
 
-		FileUtils.rm_rf(dest_path) if File.exists?(dest_path)
+    FileUtils.rm_rf(dest_path) if File.exists?(dest_path)
 
-		format.file_entries.each do |entry, file_data|
-			path = entry['path'].untaint
-			path = File.expand_path File.join(gem_dir, path)
+    format.file_entries.each do |entry, file_data|
+      path = entry['path'].untaint
+      path = File.expand_path File.join(gem_dir, path)
 
-			FileUtils.rm_rf(path) if File.exists?(path)
-			FileUtils.mkdir_p File.dirname(path)
+      FileUtils.rm_rf(path) if File.exists?(path)
+      FileUtils.mkdir_p File.dirname(path)
 
-			File.open(path, "wb") do |out|
-				out.write file_data
-			end
+      File.open(path, "wb") do |out|
+        out.write file_data
+      end
 
-			FileUtils.chmod entry['mode'], path
+      FileUtils.chmod entry['mode'], path
 
-			say path if Gem.configuration.really_verbose
-		end
+      say path if Gem.configuration.really_verbose
+    end
 
-		ran_rake = false
-		start_dir = Dir.pwd
+    ran_rake = false
+    start_dir = Dir.pwd
 
-		spec.extensions.each do |extension|
-			break if ran_rake
-			results = []
+    spec.extensions.each do |extension|
+      break if ran_rake
+      results = []
 
-			builder = case extension
-								when /extconf/ then
-									Gem::Ext::ExtConfBuilder
-								when /configure/ then
-									Gem::Ext::ConfigureBuilder
-								when /rakefile/i, /mkrf_conf/i then
-									ran_rake = true
-									Gem::Ext::RakeBuilder
-								else
-									results = ["No builder for extension '#{extension}'"]
-									raise results.last
-								end
+      builder = case extension
+                  when /extconf/ then
+                    Gem::Ext::ExtConfBuilder
+                  when /configure/ then
+                    Gem::Ext::ConfigureBuilder
+                  when /rakefile/i, /mkrf_conf/i then
+                    ran_rake = true
+                    Gem::Ext::RakeBuilder
+                  else
+                    results = ["No builder for extension '#{extension}'"]
+                    raise results.last
+                end
 
-			begin
-				build_dir = File.join(gem_dir, File.dirname(extension))
-				extension = File.expand_path(extension)
-				Dir.chdir build_dir
+      begin
+        build_dir = File.join(gem_dir, File.dirname(extension))
+        extension = File.expand_path(extension)
+        Dir.chdir build_dir
 
-				if fat_commands.empty?
-					results = builder.build(extension, gem_dir, dest_path, results)
+        if fat_commands.empty?
+          results = builder.build(extension, gem_dir, dest_path, results)
 
-				else
-					fat_commands.each_pair do |version, command|
-						dest_version_path = File.join(dest_path, version)
+        else
+          fat_commands.each_pair do |version, command|
+            dest_version_path = File.join(dest_path, version)
 
-						script = %'require "rubygems/ext"; puts #{builder}.build(#{extension.dump},#{gem_dir.dump},#{dest_version_path.dump}, [])'
+            script = %'require "rubygems/ext"; puts #{builder}.build(#{extension.dump},#{gem_dir.dump},#{dest_version_path.dump}, [])'
 
-						result = `#{command} -e '#{script}'`
-						results << result
-						raise result if $? != 0
+            result = `#{command} -e '#{script}'`
+            results << result
+            raise result if $? != 0
 
-						FileUtils.rm Dir.glob("**/*.o")    # FIXME
-					end
-				end
+            FileUtils.rm Dir.glob("**/*.o") # FIXME
+          end
+        end
 
-				say results.join("\n") if Gem.configuration.really_verbose
+        say results.join("\n") if Gem.configuration.really_verbose
 
-			rescue => ex
-				results = results.join "\n"
+      rescue => ex
+        results = results.join "\n"
 
-				File.open('gem_make.out', 'wb') {|f| f.puts results }
+        File.open('gem_make.out', 'wb') { |f| f.puts results }
 
-				message = <<-EOF
+        message = <<-EOF
 ERROR: Failed to build gem native extension.
 
 				#{results}
 
 Results logged to #{File.join(Dir.pwd, 'gem_make.out')}
-				EOF
+        EOF
 
-				raise Gem::Exception, message
+        raise Gem::Exception, message
 
-			ensure
-				Dir.chdir start_dir
-			end
-		end
+      ensure
+        Dir.chdir start_dir
+      end
+    end
 
     unless fat_commands.empty?
-			fat_ext_files = fat_commands.keys.uniq.map do |version|
-				dest_version_path = File.join(dest_path, version)
-				fat_ext_paths = Dir.glob("#{dest_version_path}/**/*")
-				fat_ext_paths.map {|path| path[File.join(dest_version_path,'').length..-1] }
-			end.flatten.uniq
+      fat_ext_files = fat_commands.keys.uniq.map do |version|
+        dest_version_path = File.join(dest_path, version)
+        fat_ext_paths = Dir.glob("#{dest_version_path}/**/*")
+        fat_ext_paths.map { |path| path[File.join(dest_version_path, '').length..-1] }
+      end.flatten.uniq
 
-			fat_ext_files.uniq.each do |ext_file|
-				ext_name = ext_file.sub(/\.[^\.]*$/, '')
-				rb_path = File.join(dest_path, "#{ext_name}.rb")
-				File.open(rb_path, "w") do |f|
-					f.write <<-EOF
+      fat_ext_files.uniq.each do |ext_file|
+        ext_name = ext_file.sub(/\.[^\.]*$/, '')
+        rb_path = File.join(dest_path, "#{ext_name}.rb")
+        File.open(rb_path, "w") do |f|
+          f.write <<-EOF
 require File.join File.dirname(__FILE__), RUBY_VERSION.match(/\\d+\\.\\d+/)[0], #{ext_name.dump}
-					EOF
-				end
-			end
-		end
+          EOF
+        end
+      end
+    end
 
-		built_paths = Dir.glob("#{dest_path}/**/*.{so,bundle}")
-		built_files = built_paths.map {|path| path[File.join(gem_dir,'').length..-1] }
+    built_paths = Dir.glob("#{dest_path}/**/*.{so,bundle}")
+    built_files = built_paths.map { |path| path[File.join(gem_dir, '').length..-1] }
 
-		spec.files = (spec.files + built_files).sort.uniq
-    spec.files = spec.files.reject{|file| file =~ /\.(c|h)$/} - spec.extensions if opts[:exclude_source] #exclude source and extension files
-		spec.platform = platform if platform
+    spec.files = (spec.files + built_files).sort.uniq
+    spec.files = spec.files.reject { |file| file =~ /\.(c|h)$/ } - spec.extensions if opts[:exclude_source] #exclude source and extension files
+    spec.platform = platform if platform
 
     spec.extensions = [] # No longer necessary to compile extensions
 
-		Dir.chdir gem_dir
-		begin
-			out_fname = Gem::Builder.new(spec).build
-			FileUtils.mv(out_fname, start_dir)
-		ensure
-			Dir.chdir start_dir
-		end
-	end
+    Dir.chdir gem_dir
+    begin
+      out_fname = Gem::Builder.new(spec).build
+      FileUtils.mv(out_fname, start_dir)
+    ensure
+      Dir.chdir start_dir
+    end
+  end
 end
 
