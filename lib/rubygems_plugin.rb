1471d7490e828943066fc3a6c3e633bafbf805a7
Pablo Baños López <pablo@besol.es>
2012-02-09 18:42:38 +0100

    Adds rubygems plugin directory to LOAD_PATH in case of exception

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,4 +1,9 @@
-require 'rubygems/commands/compile_command'
-require 'rubygems/command_manager'
-Gem::CommandManager.instance.register_command :compile
+begin
+  require 'rubygems/commands/compile_command'
+  require 'rubygems/command_manager'
+  Gem::CommandManager.instance.register_command :compile
+rescue
+  $LOAD_PATH << File.dirname(__FILE__)
+  retry
+end
 
